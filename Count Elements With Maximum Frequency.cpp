af3780cec7dd2ab852ec761d167baa6a4a318126
Abhinay-05 <abhinaynnd05@gmail.com>
2025-11-07 20:49:16 +0530

    Create Count Elements With Maximum Frequency.cpp

---
+++ Count Elements With Maximum Frequency.cpp
@@ -0,0 +1,52 @@
+/*
+3005. Count Elements With Maximum Frequency
+
+You are given an array nums consisting of positive integers.
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+The frequency of an element is the number of occurrences of that element in the array.
+
+Example 1:
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+
+Example 2:
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+ 
+Constraints:
+1 <= nums.length <= 100
+1 <= nums
+*/
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int len = nums.size();
+
+        unordered_map<int,int> map;
+        // insert in map
+        for(int i : nums){
+            map[i]++;
+        }
+
+        // find max frequency
+        int max = 0;
+        for(auto i : map){
+            if(i.second > max){
+                max = i.second;
+            }
+        }
+
+        // count elements
+        int count = 0;
+        for(auto i : map){
+            if(i.second == max){
+                count++;
+            }
+        }
+        return count*max;
+    }
+};
