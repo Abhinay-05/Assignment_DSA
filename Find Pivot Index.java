c96d534da0e7e99c0b623995b50ef60abb09b554
Abhinay-05 <abhinaynnd05@gmail.com>
2025-11-12 23:45:02 +0530

    Implement recursive pivot index calculation

--- Find Pivot Index.java
+++ Find Pivot Index.java
@@ -16,3 +16,26 @@ class Solution {
         return -1;
     }
 }
+
+class Solution1 {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int el : nums){
+            total += el;
+        }
+        return pivotIndex(nums, 0, 0, total);
+    }
+
+    private int pivotIndex(int[] nums, int i, int left, int total){
+        // no Index found
+        if(i == nums.length){
+            return -1;
+        }
+        // Index found
+        if(left == (total - nums[i] - left)){
+            return i;
+        }
+        // next index
+        return pivotIndex(nums, i+1, left+nums[i], total);
+    }
+}
