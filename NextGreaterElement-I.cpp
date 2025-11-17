669a4631a74d4762e26a87e2c22d8efca798324b
Abhinay-05 <abhinaynnd05@gmail.com>
2025-11-17 22:28:52 +0530

    Updated solution (Monotonic Stack)

--- NextGreaterElement-I.cpp
+++ NextGreaterElement-I.cpp
@@ -4,26 +4,35 @@ using namespace std;
 vector<int> nextGreaterElement(vector<int>& sub, vector<int>& arr ) {
         unordered_map<int, int> map;
         int m = sub.size(), n = arr.size();
+
         for(int i = 0 ; i < n ; i++){
-            map[arr[i]] = i;
+            map[arr[i]] = -1;
         }
 
-        for(int i = 0 ; i < m ; i++){
-            int curr = sub[i];
-            sub[i] = -1;
-            for(int j = map[curr]+1 ; j < n ; j++){
-                if(arr[j] > curr){
-                    sub[j] = arr[j];
-                    break;
-                }
+        stack<int> st;
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.empty() && st.top() < arr[i]){
+                st.pop();
             }
+
+            if(!st.empty()){
+                map[arr[i]] = st.top();
+                st.push(arr[i]);
+            }
+            else{
+                // stack is empty
+                st.push(arr[i]);
+            }
+        }
+        for(int i = 0 ; i < m ; i++){
+            sub[i] = map[sub[i]];
         }
         return sub;
     }
 
 int main(){
-    vector<int> sub = {4,1,2};
-    vector<int> arr = {1,3,4,2};
+    vector<int> arr = {5, 1, 2, 4, 3};
+    vector<int> sub(arr);
     sub = nextGreaterElement(sub, arr);
     for(int el : sub){
         cout<<el<<" ";
