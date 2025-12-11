60e728c1af8b8fec4ccac3bf9145ed3e9b21eb99
Abhinay-05 <abhinaynnd05@gmail.com>
2025-12-11 23:57:48 +0530

    added the approach to solve it

---
+++ rotatedSortedArray.java
@@ -0,0 +1,10 @@
+public class rotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        //using Binary Search approach
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+    }
+    
+}
