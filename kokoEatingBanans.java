b6261945bf1869934290e91ceb9a752a12471bfa
Abhinay-05 <abhinaynnd05@gmail.com>
2025-12-16 00:52:09 +0530

    optimal solution TC: O(Nlogm), SC: O(1)

--- kokoEatingBanans.java
+++ kokoEatingBanans.java
@@ -1,7 +1,41 @@
 public class kokoEatingBanans {
     public static int minEatingSpeed(int[] piles, int h) {
-        //use Binary Search algorithm
+        //Binary Search Approach
+        //TC: O(N*log(M))
+        //N -> number of elements in the array
+        //M -> maximum element in the array
+        
+        //find max elements
+        int max = Integer.MIN_VALUE;
+        for(int el : piles){
+            max = Math.max(el, max);
+        }
+
+        int s = 1, e = max;
+        while(s < e){
+            int m = s + (e - s)/2;
+            int count = 0;
+
+            for(int el : piles){
+                //count the number hours it takes to eat a pile
+                count = count + (el/m);
+                if((el/m) == 0 /*if less than 0*/|| (el%m) != 0/*if there is a remainder */){
+                    count++;
+                }
+            }
+            
+            if(count > h){
+                //takes more hours
+                s = m + 1;
+            }
+            else{
+                //takes less than or equals
+                e = m;
+            }
+        }
+        return s;
     }
+    
 
     public static void main(String[] args){
         int[] piles = {3,6,7,11};
